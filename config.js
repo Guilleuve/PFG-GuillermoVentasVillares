4765492f47d991a11ee0977ee3de452b90114c8a
Guilleuve <guilleuve@gmail.com>
2023-06-30 21:48:58 +0200

    v1.0

--- config.js
+++ config.js
@@ -2,5 +2,4 @@ let BASE_URL = "https://cooltrainer-2cb4079caab8.herokuapp.com/";
 if (!process.env.NODE_ENV || process.env.NODE_ENV === "development") {
   BASE_URL = "http://localhost:3000/";
 }
-
 export { BASE_URL };
