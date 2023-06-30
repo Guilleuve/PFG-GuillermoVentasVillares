4927ddcca9ac3118684a04f328adfc5be351f8f7
Guilleuve <guilleuve@gmail.com>
2023-06-30 23:03:14 +0200

    v1.0

--- server.js
+++ server.js
@@ -28,10 +28,6 @@ app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 app.use("/assets", express.static(path.join(__dirname, "public/assets")));
 
-app.use(morgan("common"));
-app.use(bodyParser.json({ limit: "30mb", extended: true }));
-app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
-
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
     cb(null, "./public/assets/");
