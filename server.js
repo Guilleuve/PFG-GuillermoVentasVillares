6b243af5417c660562884399368806766ea6d398
Guilleuve <guilleuve@gmail.com>
2023-06-30 22:55:10 +0200

    v1.0

--- server.js
+++ server.js
@@ -20,17 +20,17 @@ const __dirname = path.dirname(__filename);
 dotenv.config();
 const app = express();
 app.use(express.json());
-//app.use(helmet());
-//app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
-//app.use(morgan("common"));
-//app.use(bodyParser.json({ limit: "30mb", extended: true }));
-//app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
-//app.use(cors());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(morgan("common"));
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
 app.use("/assets", express.static(path.join(__dirname, "public/assets")));
 
-//app.use(morgan("common"));
-//app.use(bodyParser.json({ limit: "30mb", extended: true }));
-//app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(morgan("common"));
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
