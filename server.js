23cbbabc329c7829d78c0fb5c698a34334f20510
Guilleuve <guilleuve@gmail.com>
2023-07-01 19:50:22 +0200

    heroku

--- server.js
+++ server.js
@@ -21,7 +21,6 @@ const __dirname = path.dirname(__filename);
 dotenv.config();
 const app = express();
 app.use(express.json());
-app.use(helmet());
 app.use(
   helmet({
     contentSecurityPolicy: false,
