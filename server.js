3bb8598b4e8ee98a1c58ea0147187c28d262a44a
Guilleuve <guilleuve@gmail.com>
2023-07-01 19:57:55 +0200

    heroku

--- server.js
+++ server.js
@@ -29,7 +29,12 @@ app.use(
 app.use(morgan("common"));
 //app.use(bodyParser.json({ limit: "30mb", extended: true }));
 //app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
-app.use(cors());
+app.use(cors({
+  origin: "*",
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  preflightContinue: false,
+  optionsSuccessStatus: 204,
+}));
 app.use("/assets", express.static(path.join(__dirname, "public/assets")));
 
 //app.use(morgan("common"));
