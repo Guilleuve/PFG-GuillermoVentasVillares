b39253699536cb086dba9e41ad235b6dc62d90c2
Guilleuve <guilleuve@gmail.com>
2023-07-01 19:44:10 +0200

    heroku

--- server.js
+++ server.js
@@ -27,7 +27,6 @@ app.use(
     contentSecurityPolicy: false,
   })
 );
-app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use(morgan("common"));
 //app.use(bodyParser.json({ limit: "30mb", extended: true }));
 //app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
