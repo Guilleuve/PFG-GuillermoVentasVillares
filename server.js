5b8386c44ca01c8c21b5acd8951c885bb4603f7d
Guilleuve <guilleuve@gmail.com>
2023-07-01 20:13:36 +0200

    heroku

--- server.js
+++ server.js
@@ -21,20 +21,12 @@ const __dirname = path.dirname(__filename);
 dotenv.config();
 const app = express();
 app.use(express.json());
-app.use(
-  helmet({
-    contentSecurityPolicy: false,
-  })
-);
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use(morgan("common"));
 //app.use(bodyParser.json({ limit: "30mb", extended: true }));
 //app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
-app.use(cors({
-  origin: "*",
-  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-  preflightContinue: false,
-  optionsSuccessStatus: 204,
-}));
+app.use(cors());
 app.use("/assets", express.static(path.join(__dirname, "public/assets")));
 
 //app.use(morgan("common"));
