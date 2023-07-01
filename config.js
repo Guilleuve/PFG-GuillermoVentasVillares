58b214ea08577d2920a9666608acb091614ca359
Guilleuve <guilleuve@gmail.com>
2023-07-01 20:05:43 +0200

    heroku

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-let BASE_URL = "https://cooltrainer-2cb4079caab8.herokuapp.com/";
+let BASE_URL = "https://cooltrainer.herokuapp.com/";
 if (!process.env.NODE_ENV || process.env.NODE_ENV === "development") {
   BASE_URL = "http://localhost:3000/";
 }
